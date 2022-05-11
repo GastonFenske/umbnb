5669c875ef2c2e7c1364ae857dd5791594a00b9e
Julian <julyplottier@hotmail.com>
2022-05-11 18:05:44 -0300

    Initial commit, upload Spring Boot files

---
+++ src/main/java/com/um/umbnb/UmbnbApplication.java
@@ -0,0 +1,13 @@
+package com.um.umbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/um/umbnb/UmbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um.umbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
